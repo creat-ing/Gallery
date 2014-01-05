71eb75e51c3757c9bfe0a2e5a6c877bb799e2b63
creat-ing <jonas.maechler@bluewin.ch>
2014-01-05 06:14:06 +0100

    Update upload.php

--- core/components/gallery/processors/mgr/item/upload.php
+++ core/components/gallery/processors/mgr/item/upload.php
@@ -61,11 +61,11 @@ if (!$item->upload($_FILES['file'],$scriptProperties['album'])) {
 $item->save();
 
 /* get rank of last item, add 1 */
-$c = $this->modx->newQuery('galAlbumItem');
+$c = $modx->newQuery('galAlbumItem');
 $c->where(array('album' => $album));
 $c->sortby('rank','DESC');
 $c->limit(1);
-$maxRankItem = $this->modx->getObject('galAlbumItem',$c);
+$maxRankItem = $modx->getObject('galAlbumItem',$c);
 $total = $maxRankItem->get('rank')+1;
 
 /* associate with album */
